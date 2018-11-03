35de71c8f7d4992319bed87ad640caf17815f472
Josh Mcguigan <joshmcg88@gmail.com>
2018-11-03 15:59:23 -0700

    Added debounce time between triggers

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::udp::UdpPacket;
 use pnet::datalink::{self, NetworkInterface};
+use std::time::SystemTime;
+use std::time::Duration;
+use std::time::UNIX_EPOCH;
 
 fn main() {
     use pnet::datalink::Channel::Ethernet;
@@ -32,17 +35,19 @@ fn main() {
         Err(e) => panic!("dashr: unable to create channel: {}", e),
     };
 
+    let mut last_trigger_time = Duration::new(0, 0);
+
     loop {
         match rx.next() {
             Ok(packet) => {
-                handle_ethernet_frame(&EthernetPacket::new(packet).unwrap());
+                handle_ethernet_frame(&EthernetPacket::new(packet).unwrap(), &mut last_trigger_time);
             },
             Err(e) => panic!("dashr: unable to receive packet: {}", e),
         }
     }
 }
 
-fn handle_ethernet_frame(ethernet: &EthernetPacket) {
+fn handle_ethernet_frame(ethernet: &EthernetPacket, last_trigger_time: &mut Duration) {
     if ethernet.get_ethertype() == EtherTypes::Ipv4 {
         let header = Ipv4Packet::new(ethernet.payload());
         if let Some(header) = header {
@@ -50,11 +55,21 @@ fn handle_ethernet_frame(ethernet: &EthernetPacket) {
             let udp = UdpPacket::new(packet);
             if let Some(udp) = udp {
                 if udp.get_source() == 68 && udp.get_destination() == 67 {
-                    print_client_mac_from_dhcp_discovery_packet(packet);
+                    let debouce_time = 2; // debounce time in seconds TODO this could be a command line arg
+                    if last_trigger_time.as_secs() + debouce_time < get_current_time().as_secs() {
+                        *last_trigger_time = get_current_time();
+                        print_client_mac_from_dhcp_discovery_packet(packet);
+                    }
+
                 }
             }
         }
     }
+}
+
+fn get_current_time() -> Duration {
+    SystemTime::now().duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
 }
 
 fn print_client_mac_from_dhcp_discovery_packet(packet: &[u8]){
