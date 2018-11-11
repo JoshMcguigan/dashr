806bee1cbe90e67bf1ea966726958656d187985e
Josh Mcguigan <joshmcg88@gmail.com>
2018-11-11 13:03:22 -0700

    Converted to structop for arg parsing, allow specifying delay time

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
 extern crate pnet;
 
-use std::io::{self, Write};
-use std::env;
-use std::process;
 use pnet::packet::Packet;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::udp::UdpPacket;
-use pnet::datalink::{self, NetworkInterface};
-use std::time::SystemTime;
-use std::time::Duration;
-use std::time::UNIX_EPOCH;
+use pnet::datalink::{self, NetworkInterface, Channel::Ethernet};
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Network interface
+    network_interface: String,
+
+    /// Delay time (seconds) between presses
+    #[structopt(short = "d", long = "delay", default_value = "5")]
+    delay: u64,
+}
 
 fn main() {
-    use pnet::datalink::Channel::Ethernet;
+    let opt = Opt::from_args();
 
-    let iface_name = match env::args().nth(1) {
-        Some(n) => n,
-        None => {
-            writeln!(io::stderr(), "USAGE: dashr <NETWORK INTERFACE>").unwrap();
-            process::exit(1);
-        },
-    };
-    let interface_names_match = |iface: &NetworkInterface| iface.name == iface_name;
+    let interface_names_match = |iface: &NetworkInterface| iface.name == opt.network_interface;
 
     // Find the network interface with the provided name
     let interfaces = datalink::interfaces();
@@ -31,7 +30,7 @@ fn main() {
     // Create a channel to receive on
     let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("dashr: unhandled channel type: {}"),
+        Ok(_) => panic!("dashr: unhandled channel type"),
         Err(e) => panic!("dashr: unable to create channel: {}", e),
     };
 
@@ -40,14 +39,14 @@ fn main() {
     loop {
         match rx.next() {
             Ok(packet) => {
-                handle_ethernet_frame(&EthernetPacket::new(packet).unwrap(), &mut last_trigger_time);
+                handle_ethernet_frame(&EthernetPacket::new(packet).unwrap(), &mut last_trigger_time, opt.delay);
             },
             Err(e) => panic!("dashr: unable to receive packet: {}", e),
         }
     }
 }
 
-fn handle_ethernet_frame(ethernet: &EthernetPacket, last_trigger_time: &mut Duration) {
+fn handle_ethernet_frame(ethernet: &EthernetPacket, last_trigger_time: &mut Duration, delay: u64) {
     if ethernet.get_ethertype() == EtherTypes::Ipv4 {
         let header = Ipv4Packet::new(ethernet.payload());
         if let Some(header) = header {
@@ -55,8 +54,7 @@ fn handle_ethernet_frame(ethernet: &EthernetPacket, last_trigger_time: &mut Dura
             let udp = UdpPacket::new(packet);
             if let Some(udp) = udp {
                 if udp.get_source() == 68 && udp.get_destination() == 67 {
-                    let debouce_time = 2; // debounce time in seconds TODO this could be a command line arg
-                    if last_trigger_time.as_secs() + debouce_time < get_current_time().as_secs() {
+                    if last_trigger_time.as_secs() + delay < get_current_time().as_secs() {
                         *last_trigger_time = get_current_time();
                         print_client_mac_from_dhcp_discovery_packet(packet);
                     }
